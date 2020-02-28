c865e25bad19ded4944400e7c82031b1d9bcbabf
Steffen Jacobs <steffen.jacobs@hotmail.de>
2020-02-28 22:51:53 +0100

    feat: add helper api call to exchange a user token for a Page API token

--- src/main/java/me/steffenjacobs/supersocial/service/FacebookService.java
+++ src/main/java/me/steffenjacobs/supersocial/service/FacebookService.java
@@ -7,6 +7,7 @@ import java.nio.charset.StandardCharsets;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -28,6 +29,7 @@ public class FacebookService {
 	private CredentialService credentialService;
 
 	private static final String FACEBOOK_POST_TO_PAGE_ENDPOINT = "https://graph.facebook.com/%s/feed?message=%s&access_token=%s";
+	private static final String FACEBOOK_EXCHANGE_TO_PAGE_TOKEN_ENDPOINT = "https://graph.facebook.com/%s?access_token=%s&fields=access_token";
 
 	/**
 	 * Publishes {@code text} with the credentials given in the
@@ -44,7 +46,7 @@ public class FacebookService {
 
 	/**
 	 * Publishes {@code text} with the credentials given in the
-	 * credentials.properties file to the given facebook page.
+	 * credentials.properties file to the given Facebook page.
 	 */
 	private void attemptPostMessage(String text) throws UnsupportedOperationException, IOException {
 		final HttpPost httpPost = new HttpPost(String.format(FACEBOOK_POST_TO_PAGE_ENDPOINT, credentialService.getCredential(Credential.FACEBOOK_PAGE_ID),
@@ -53,6 +55,22 @@ public class FacebookService {
 		try (final CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
 			final HttpResponse httpResponse = httpClient.execute(httpPost);
 			LOG.info(IOUtils.toString(httpResponse.getEntity().getContent(), Charset.forName("UTF-8")));
+		}
+	}
+
+	/**
+	 * Exchange an {@code userToken} for a page token using the Facebook Graph
+	 * API. @return the result from the Facebook API.
+	 */
+	public String exchangeForPageToken(String userToken) {
+		final HttpGet httpGet = new HttpGet(String.format(FACEBOOK_EXCHANGE_TO_PAGE_TOKEN_ENDPOINT, credentialService.getCredential(Credential.FACEBOOK_PAGE_ID),
+				URLEncoder.encode(userToken, StandardCharsets.UTF_8)));
+
+		try (final CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+			HttpResponse httpResponse = httpClient.execute(httpGet);
+			return IOUtils.toString(httpResponse.getEntity().getContent(), Charset.forName("UTF-8"));
+		} catch (IOException e1) {
+			throw new FacebookException("Could not exchange user token for page token", e1);
 		}
 	}
 
