aefd14da89e2761b8a8b5517144327057d187ee4
Steffen Jacobs <steffen.jacobs@hotmail.de>
2020-03-15 13:02:25 +0100

    feat: return correct http error codeif a post is not found

--- backend/supersocial-webapp/src/main/java/me/steffenjacobs/supersocial/PostController.java
+++ backend/supersocial-webapp/src/main/java/me/steffenjacobs/supersocial/PostController.java
@@ -3,12 +3,15 @@ package me.steffenjacobs.supersocial;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import me.steffenjacobs.supersocial.domain.dto.PostDTO;
 import me.steffenjacobs.supersocial.persistence.PostPersistenceManager;
+import me.steffenjacobs.supersocial.service.exception.PostNotFoundException;
 
 /** @author Steffen Jacobs */
 
@@ -24,7 +27,11 @@ public class PostController {
 	}
 
 	@GetMapping(path = "/api/post/{id}")
-	public PostDTO getPublishedPostById(@PathVariable long id) {
-		return postPersistenceManager.findPostById(id);
+	public ResponseEntity<PostDTO> getPublishedPostById(@PathVariable(name="id") long id) {
+		try {
+			return new ResponseEntity<>(postPersistenceManager.findPostById(id), HttpStatus.OK);
+		} catch (PostNotFoundException e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 	}
 }
