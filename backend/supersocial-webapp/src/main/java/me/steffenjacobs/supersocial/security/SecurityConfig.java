b597eaac45d8f22680e0692956999b635a40a31a
Steffen Jacobs <steffen.jacobs@hotmail.de>
2020-03-15 12:41:25 +0100

    refactor: move SecurityConfig into security package

--- backend/supersocial-webapp/src/main/java/me/steffenjacobs/supersocial/security/SecurityConfig.java
+++ backend/supersocial-webapp/src/main/java/me/steffenjacobs/supersocial/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package me.steffenjacobs.supersocial.configuration;
+package me.steffenjacobs.supersocial.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
