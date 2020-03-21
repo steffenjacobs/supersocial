3ffe78824b70e80bdc5f3ee02da337d276460a0d
Steffen Jacobs <steffen.jacobs@hotmail.de>
2020-03-21 18:03:28 +0100

    feat(frontend): add sidebar

---
+++ frontend/src/AboutPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class AboutPage extends React.Component<any, any>{
+
+    public render() {
+        return (
+            <div>
+                <div>About</div>
+                <div>by Steffen Jacobs</div>
+            </div>
+        );
+    }
+}
---
+++ frontend/src/Sidebar.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import './Sidebar.css';
+
+export interface PageComponents {
+    components: PageComponent[]
+}
+
+export interface PageComponent {
+    id: number
+    title: string
+    page: JSX.Element
+    icon: JSX.Element
+    selected?: boolean
+}
+
+export class Sidebar extends React.Component<PageComponents, PageComponents>{
+    constructor(props: PageComponents, state: PageComponents) {
+        super(props);
+        this.state = props;
+    }
+
+    private setActivePage(pageId: number) {
+        var newComponents = [];
+        for (let c = 0; c < this.state.components.length; c++) {
+            let comp = this.state.components[c];
+            if (comp.selected && comp.id !== pageId) {
+                newComponents.push({ id: comp.id, title: comp.title, page: comp.page, icon: comp.icon })
+            }
+            else if (comp.id === pageId) {
+                newComponents.push({ id: comp.id, title: comp.title, page: comp.page, icon: comp.icon, selected: true })
+            } else {
+                newComponents.push(comp);
+            }
+        }
+
+        this.setState({ components: newComponents });
+        //TODO: set title and url
+    }
+
+    public render() {
+        const components = this.state.components.map((elem) => {
+            const clazz = elem.selected ? 'navbar-menuItem navbar-menuItem-active' : 'navbar-menuItem';
+            return (
+                <a onClick={() => this.setActivePage(elem.id)}>
+                    <div className={clazz}>
+                        {elem.icon}
+                        <div className="navbar-text">{elem.title}</div>
+                    </div>
+                </a>
+            );
+        });
+
+        const selectedComponent = this.state.components.find((c) => { return c.selected })?.page;
+
+        return (
+            <div>
+                <div className="navbar">
+                    <div className="navbar-logo">
+                        <img src="/logo192.png" />
+                        <div className="navbar-brandname"> Supersocial</div>
+                    </div>
+                    {components}
+                </div>
+                <div className="navbar-page">{selectedComponent}</div>
+            </div>
+        );
+    }
+}
