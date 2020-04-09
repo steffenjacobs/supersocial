51d5eb5d5f5561818cf8e4ee3081a2056a592b47
Steffen Jacobs <steffen.jacobs@hotmail.de>
2020-04-09 20:08:05 +0200

    feat(SUP-8, backend): Add version endpoint

--- backend/src/main/java/me/steffenjacobs/supersocial/endpoints/LoginInfoController.java
+++ backend/src/main/java/me/steffenjacobs/supersocial/endpoints/LoginInfoController.java
@@ -1,6 +1,8 @@
 package me.steffenjacobs.supersocial.endpoints;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +11,22 @@ import me.steffenjacobs.supersocial.security.SecurityService;
 
 /** @author Steffen Jacobs */
 @RestController
+@PropertySource("classpath:application.properties")
 public class LoginInfoController {
 	
 	@Autowired
 	private SecurityService securityService;
+	
+	@Value("${app.version:unknown}")
+	private String version;
 
 	@GetMapping(path = "/api/loginstatus")
 	public CurrentUserDTO getLoginStatus() throws Exception {
 		return CurrentUserDTO.fromUser(securityService.getCurrentUser());
+	}
+	
+	@GetMapping(path = "/api/version")
+	public String getVersion() throws Exception {
+		return "{\"version:\": " + version + "\"}";
 	}
 }
