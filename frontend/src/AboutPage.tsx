9974b3816ffd957ff7261921045f27e463a12ef4
Steffen Jacobs <steffen.jacobs@hotmail.de>
2020-04-16 17:32:52 +0200

    chore(frontend): add more social media links to about page

--- frontend/src/AboutPage.tsx
+++ frontend/src/AboutPage.tsx
@@ -34,10 +34,10 @@ export class AboutPage extends React.Component<any, AboutState> {
         return (
             <div className="about-container">
                 <h1>Supersocial by </h1>
-                <AboutTile name="Steffen Jacobs" role="Project Manager and Lead Developer" twitterHandle="Steffen_Jacobs_" facebookHandle="steffen.jacobs.796" linkedinHandle="steffen-jacobs" xingHandle="Steffen_Jacobs7" githubHandle="steffenjacobs" description="Hi, I'm Steffen. I am a consultant and software engineer and work on all sorts of interesting private and professional projects. I lead this project and also contributed most of the initial source code. I love to work with recent technologies and learn new stuff." imageUrl="team/steffen.JPG" />
+                <AboutTile name="Steffen Jacobs" role="Product Owner and Lead Developer" tiktokHandle="steffen.jacobs" instagramHandle="steffen16378" twitterHandle="Steffen_Jacobs_" facebookHandle="steffen.jacobs.796" linkedinHandle="steffen-jacobs" xingHandle="Steffen_Jacobs7" githubHandle="steffenjacobs" description="Hi, I'm Steffen. I am a consultant and software engineer and work on all sorts of interesting private and professional entrepreneurial projects. I love to work with likeminded individuals who have a faible for recent technologies and learning new stuff. If you'd like to work with us and have the required entrepreneurial sprit, feel free to message me." imageUrl="team/steffen.JPG" />
                 <AboutTile name="Sebastian Hädrich" role="Frontend Developer" description="This is me!" imageUrl="team/placeholder.svg" />
                 <AboutTile name="Thomas Kruse" role="Developer" description="This is me!" imageUrl="team/placeholder.svg" />
-                <div>Product version: {this.state.version}</div>
+                <div>Product version: Supersocial v{this.state.version}</div>
             </div>
         );
     }
--- frontend/src/AboutTile.tsx
+++ frontend/src/AboutTile.tsx
@@ -11,6 +11,8 @@ export interface ProjectMemberProps {
     linkedinHandle?: string
     xingHandle?: string
     githubHandle?: string
+    instagramHandle?: string
+    tiktokHandle?: string
 }
 
 export class AboutTile extends React.Component<ProjectMemberProps>{
@@ -24,12 +26,14 @@ export class AboutTile extends React.Component<ProjectMemberProps>{
                     <div className="about-entry-left-inner">
                         <div className="about-entry-description">{this.props.description}</div>
                         <div >
-                            {(this.props.twitterHandle || this.props.facebookHandle || this.props.linkedinHandle || this.props.xingHandle || this.props.githubHandle) && <span className="about-entry-socialmediatext">Follow me on social media:</span>}
+                            {(this.props.twitterHandle || this.props.facebookHandle || this.props.linkedinHandle || this.props.xingHandle || this.props.githubHandle || this.props.instagramHandle ||this.props.tiktokHandle) && <span className="about-entry-socialmediatext">Follow me on social media:</span>}
                             {this.props.twitterHandle && <a target="_blank" rel="noopener noreferrer" className="about-entry-socialmedia-icon" href={"https://twitter.com/" + this.props.twitterHandle}>{ImageProvider.getSocialmediaIcon(2)}</a>}
                             {this.props.facebookHandle && <a target="_blank" rel="noopener noreferrer" className="about-entry-socialmedia-icon" href={"https://facebook.com/" + this.props.facebookHandle}>{ImageProvider.getSocialmediaIcon(1)}</a>}
                             {this.props.linkedinHandle && <a target="_blank" rel="noopener noreferrer" className="about-entry-socialmedia-icon" href={"https://www.linkedin.com/in/" + this.props.linkedinHandle}>{ImageProvider.getSocialmediaIcon(3)}</a>}
                             {this.props.xingHandle && <a target="_blank" rel="noopener noreferrer" className="about-entry-socialmedia-icon" href={"https://www.xing.com/profile/" + this.props.xingHandle}>{ImageProvider.getSocialmediaIcon(4)}</a>}
                             {this.props.githubHandle && <a target="_blank" rel="noopener noreferrer" className="about-entry-socialmedia-icon" href={"https://github.com/" + this.props.githubHandle}>{ImageProvider.getSocialmediaIcon(1001)}</a>}
+                            {this.props.instagramHandle && <a target="_blank" rel="noopener noreferrer" className="about-entry-socialmedia-icon" href={"https://www.instagram.com/" + this.props.instagramHandle}>{ImageProvider.getSocialmediaIcon(5)}</a>}
+                            {this.props.tiktokHandle && <a target="_blank" rel="noopener noreferrer" className="about-entry-socialmedia-icon" href={"https://www.tiktok.com/@" + this.props.tiktokHandle}>{ImageProvider.getSocialmediaIcon(6)}</a>}
                         </div>
                     </div>
                 </div>
