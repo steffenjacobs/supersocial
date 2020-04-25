852c886e73d65557cbf068f32f21de2a2ed9bc22
Steffen Jacobs <steffen.jacobs@hotmail.de>
2020-04-25 13:59:28 +0200

    feat(SUP-9, frontend): add link to data privacy page from about page

--- frontend/src/AboutPage.tsx
+++ frontend/src/AboutPage.tsx
@@ -37,8 +37,9 @@ export class AboutPage extends React.Component<any, AboutState> {
                 <AboutTile name="Steffen Jacobs" role="Product Owner and Lead Developer" tiktokHandle="steffen.jacobs" instagramHandle="steffen16378" twitterHandle="Steffen_Jacobs_" facebookHandle="steffen.jacobs.796" linkedinHandle="steffen-jacobs" xingHandle="Steffen_Jacobs7" githubHandle="steffenjacobs" description="Hi, I'm Steffen. I am a consultant and software engineer and work on all sorts of interesting private and professional entrepreneurial projects. I love to work with likeminded individuals who have a faible for recent technologies and learning new stuff. If you'd like to work with us and have the required entrepreneurial sprit, feel free to message me." imageUrl="team/steffen.JPG" />
                 <AboutTile name="Sebastian Hädrich" role="Frontend Developer" description="This is me!" imageUrl="team/placeholder.svg" />
                 <AboutTile name="Thomas Kruse" role="Developer" description="This is me!" imageUrl="team/placeholder.svg" />
-                <div>Product version: Supersocial v{this.state.version}</div>
+                <button onClick={e=>window.location.href="/privacy"} className="btn btn-primary landing-btn-header">Data Privacy &nbsp;&#9654;</button>
+                <div className="about-version">Product version: Supersocial v{this.state.version}</div>
             </div>
         );
-    }
+    } 
 }
